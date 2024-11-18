4a3d5f1ebd8c766140ebcdc6fd960b5e96c71af1
Kim Chanho <beroso20th@gmail.com>
2024-11-18 18:52:43 +0900

    feat : 네트워크강제종료

---
+++ src/network/exception/close/reset/ResetCloseServer.java
@@ -0,0 +1,21 @@
+package network.exception.close.reset;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class ResetCloseServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(12345);
+        Socket socket = serverSocket.accept();
+        log("소켓 연결: " + socket);
+
+        Thread.sleep(100000);
+
+        socket.close();
+        serverSocket.close();
+        log("소켓 종료");
+    }
+}
